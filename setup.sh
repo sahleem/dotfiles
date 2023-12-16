0379780f78b422f1496b32f5f0d86885867436a9
nunqi <61606327+nunqi@users.noreply.github.com>
2023-12-16 01:40:17 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo sed -i '/^\[multilib\]/{n;s/^#//}' /etc/pacman.conf
 sudo pacman -Syu
 
 # install requirements
-sudo pacman -S $(cat install.txt)
+sudo pacman -S $(cat pacman-packages.txt)
 
 # change shell to ZSH
 chsh -s $(which zsh)
