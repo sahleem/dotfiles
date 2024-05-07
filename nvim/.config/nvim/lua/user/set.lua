1d3d4303426a0588d3c0147cd97d897a5f9a18ce
Emanuel <emanuel10.01@gmail.com>
2024-05-07 11:09:35 -0300

    Nvim show space and eol

--- nvim/.config/nvim/lua/user/set.lua
+++ nvim/.config/nvim/lua/user/set.lua
@@ -11,6 +11,12 @@ set.expandtab = true
 
 set.smartindent = true
 
+set.list = true
+set.listchars:append({
+    space = "·",
+    eol = "¬"
+})
+
 set.swapfile = false
 set.backup = false
 set.undodir = os.getenv("HOME") .. "/.vim/undodir"
