409c29f2a765f5007bf54398bf75f4c7d414950a
sahleem <61606327+sahleem@users.noreply.github.com>
2025-09-30 12:55:58 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 # add apk packages
-cat apk-packages.txt | doas apk add
+#cat apk-packages.txt | sudo apk add
 
 # change shell to ZSH
 chsh -s $(which zsh)
 
+# setup autologin
+mkdir /etc/systemd/system/getty@tty1.service.d/
+cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
+
 # link config
-doas ln -sf $PWD/profile /etc/profile
-ln -sf $PWD/kak/ $HOME/.config/
-ln -sf $PWD/niri/ $HOME/.config/
-#ln -sf $PWD/zsh/.* $HOME/
+#sudo ln -sf $PWD/profile /etc/profile
+ln -sf $PWD/kak/     $HOME/.config/
+ln -sf $PWD/niri/    $HOME/.config/
+ln -sf $PWD/zsh/.*   $ZDOTDIR/
