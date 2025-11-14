9b30e5ba81231412a2c1fbb902cbf0b7ba4162e5
sahleem <61606327+sahleem@users.noreply.github.com>
2025-11-14 01:43:34 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@
 # add apk packages
 #cat apk-packages.txt | sudo apk add
 
-# change shell to ZSH
-chsh -s $(which zsh)
+# change shell to FISH
+chsh -s $(which fish)
 
 # setup autologin
-mkdir /etc/systemd/system/getty@tty1.service.d/
-cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
+sudo mkdir /etc/systemd/system/getty@tty1.service.d/
+sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
 
 # link config
 #sudo ln -sf $PWD/profile /etc/profile
 ln -sf $PWD/kak/     $HOME/.config/
 ln -sf $PWD/niri/    $HOME/.config/
-ln -sf $PWD/zsh/.*   $ZDOTDIR/
+ln -sf $PWD/foot/    $HOME/.config/
+ln -sf $PWD/fish/    $HOME/.config/
+#ln -sf $PWD/zsh/.*   $ZDOTDIR/
