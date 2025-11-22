b5cfb6d74ff977805f9f5d8358b45140fc8c2221
sahleem <61606327+sahleem@users.noreply.github.com>
2025-11-22 19:44:25 -0300

    Script use ZSH

--- setup.sh
+++ setup.sh
@@ -4,16 +4,15 @@
 #cat apk-packages.txt | sudo apk add
 
 # change shell to FISH
-chsh -s $(which fish)
+chsh -s $(which zsh)
 
 # setup autologin
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
 sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
 
 # link config
-#sudo ln -sf $PWD/profile /etc/profile
 ln -sf $PWD/kak/     $HOME/.config/
 ln -sf $PWD/niri/    $HOME/.config/
 ln -sf $PWD/foot/    $HOME/.config/
 ln -sf $PWD/fish/    $HOME/.config/
-#ln -sf $PWD/zsh/.*   $ZDOTDIR/
+ln -sf $PWD/zsh/.*   $ZDOTDIR/
