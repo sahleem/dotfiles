b859a4908c6d817a3dbd3c08d055e882d8a2088b
sahleem <61606327+sahleem@users.noreply.github.com>
2025-11-24 20:15:48 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ sudo mkdir /etc/systemd/system/getty@tty1.service.d/
 sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
 
 # link config
+CONFIG_DIR=$HOME/.config
 ln -sf $PWD/kak/     $CONFIG_DIR/
 ln -sf $PWD/niri/    $CONFIG_DIR/
 ln -sf $PWD/foot/    $CONFIG_DIR/
