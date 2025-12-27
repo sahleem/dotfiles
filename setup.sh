9e5db8c584b0cf1832d8501b993a99f8270528e9
sahleem <61606327+sahleem@users.noreply.github.com>
2025-12-27 00:40:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ sudo pacman -S $(cat packages.txt)
 # change shell to ZSH
 chsh -s $(which zsh)
 
+# setup zsh
+cp zsh/zprofile ~/.zprofile
+cp zsh/zshrc ~/.zshrc
+
 # setup autologin
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
 sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
