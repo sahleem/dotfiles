c59367706a84410c6a2187baef58ad61e5782870
sahleem <61606327+sahleem@users.noreply.github.com>
2026-06-19 10:59:40 -0300

    add -r to cp

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/
 # copy config
 CONFIG_DIR=$HOME/.config
 mkdir -p $CONFIG_DIR
-cp zsh/zprofile  $HOME/.zprofile
-cp zsh/zshrc     $HOME/.zshrc
-cp kak/          $CONFIG_DIR/
-cp niri/         $CONFIG_DIR/
-cp foot/         $CONFIG_DIR/
-#cp fish/         $CONFIG_DIR/
+cp -r zsh/zprofile  $HOME/.zprofile
+cp -r zsh/zshrc     $HOME/.zshrc
+cp -r kak/          $CONFIG_DIR/
+cp -r niri/         $CONFIG_DIR/
+cp -r foot/         $CONFIG_DIR/
+#cp -r fish/         $CONFIG_DIR/
